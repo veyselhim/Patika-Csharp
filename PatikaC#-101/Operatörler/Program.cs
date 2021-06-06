d634257da734bfe023b196d8162649ea91dc099b
Veysel <71026333+veyselhim@users.noreply.github.com>
2021-06-06 21:31:42 +0300

    Ödev 3

---
+++ PatikaC#-101/Operatörler/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Operatörler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("*******Atama İşlemleri********");
+            int x = 5;
+            int y = 2;
+
+            y = y + 2;
+
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            x *= 2;
+            Console.WriteLine(y);
+            x *= 2;
+
+
+            Console.WriteLine("*******Mantıksal İşlemler********");
+
+            bool isSuccess = true;
+            bool isComplated = false;
+
+            if (isSuccess && isComplated)
+                Console.WriteLine("Perfect");
+
+            if (isSuccess || isComplated)
+                Console.WriteLine("Great");
+
+            if (isSuccess && !isComplated)
+                Console.WriteLine("Fine");
+
+                        Console.WriteLine("*******İlişkisel Operatörler********");
+
+
+            int a = 5;
+            int b = 6;
+
+            bool sonuc = a < b;
+
+            Console.WriteLine(sonuc);
+            sonuc = a>b;
+
+             Console.WriteLine(sonuc);
+            sonuc = a>=b;
+
+             Console.WriteLine(sonuc);
+            sonuc = a<=b;
+
+             Console.WriteLine(sonuc);
+            sonuc = a==b;
+
+             Console.WriteLine(sonuc);
+            sonuc = a!=b;
+
+
+            Console.WriteLine("*******Aritmetik Operatörler********");
+
+            int sayi1 = 10;
+            int sayi2 = 5;
+            int sonuc1 = sayi1/sayi2;
+
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1*sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1+sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1++;
+            Console.WriteLine(sonuc1);
+            int sonuc2 = 20%3;
+            Console.WriteLine(sonuc2);
+
+        }
+    }
+}
